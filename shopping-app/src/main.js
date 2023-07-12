3106de3fd9df97122f32d7a1bd2c8889e1797474
Ayaanshaikh20 <email@youremail.com>
2023-07-12 12:09:12 +0530

    setup done

--- shopping-app/src/main.js
+++ shopping-app/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Route from './route.js'
+import Store from './store.js'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  Route,
+  Store
 }).$mount('#app')
